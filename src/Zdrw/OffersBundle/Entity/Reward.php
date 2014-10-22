2067f77fc2478211ccba28f6cbdd0a4c07a37828
Steeler <ugniushead@gmail.com>
2014-10-22 22:04:18 +0300

    Creating Reward entity and RewardRepository

---
+++ src/Zdrw/OffersBundle/Entity/Reward.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Zdrw\OffersBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Reward
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Zdrw\OffersBundle\Entity\RewardRepository")
+ */
+class Reward
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="user_id", type="integer")
+     */
+    private $userId;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="offer_id", type="integer")
+     */
+    private $offerId;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="points", type="integer")
+     */
+    private $points;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     * @return Reward
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return integer 
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set offerId
+     *
+     * @param integer $offerId
+     * @return Reward
+     */
+    public function setOfferId($offerId)
+    {
+        $this->offerId = $offerId;
+
+        return $this;
+    }
+
+    /**
+     * Get offerId
+     *
+     * @return integer 
+     */
+    public function getOfferId()
+    {
+        return $this->offerId;
+    }
+
+    /**
+     * Set points
+     *
+     * @param integer $points
+     * @return Reward
+     */
+    public function setPoints($points)
+    {
+        $this->points = $points;
+
+        return $this;
+    }
+
+    /**
+     * Get points
+     *
+     * @return integer 
+     */
+    public function getPoints()
+    {
+        return $this->points;
+    }
+}
