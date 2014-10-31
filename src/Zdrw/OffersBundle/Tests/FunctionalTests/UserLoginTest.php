94bd408035f47c9c13e02284978e7dafb9735377
Ugnius Sinonis <ugniushead@gmail.com>
2014-10-31 12:15:01 +0200

    Fixing login functional test(adding fixture loading); Removing unnecessary files; Expanding .gitignore directions list

--- src/Zdrw/OffersBundle/Tests/FunctionalTests/UserLoginTest.php
+++ src/Zdrw/OffersBundle/Tests/FunctionalTests/UserLoginTest.php
@@ -3,9 +3,32 @@
 namespace Zdrw\OffersBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Zdrw\OffersBundle\ZdrwOffersBundle;
+use Zdrw\UserBundle\ZdrwUserBundle;
 
 class UserLoginTest extends WebTestCase
 {
+    private $em;
+
+    public function setUp() {
+
+        $client = self::createClient();
+        $container = $client->getKernel()->getContainer();
+        $em = $container->get('doctrine')->getManager();
+
+        // Purge tables
+        $purger = new \Doctrine\Common\DataFixtures\Purger\ORMPurger($em);
+        $executor = new \Doctrine\Common\DataFixtures\Executor\ORMExecutor($em, $purger);
+        $executor->purge();
+
+        // Load fixtures
+        $loader = new \Doctrine\Common\DataFixtures\Loader;
+        $fixtures = new \Zdrw\UserBundle\DataFixtures\ORM\LoadUserData;
+        $fixtures->setContainer($container);
+        $loader->addFixture($fixtures);
+        $executor->execute($loader->getFixtures());
+    }
+
     public function testLogin()
     {
         $client = static::createClient();
--- src/Zdrw/UserBundle/DataFixtures/ORM/LoadUserData.php
+++ src/Zdrw/UserBundle/DataFixtures/ORM/LoadUserData.php
@@ -4,22 +4,35 @@ namespace Zdrw\UserBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use Zdrw\UserBundle\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class LoadUserData implements FixtureInterface
 {
     /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
      * {@inheritDoc}
      */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     public function load(ObjectManager $manager)
     {
-        $userAdmin = new User();
-        $userAdmin->setUsername('admin');
-        $userAdmin->setPlainPassword('test');
-        $userAdmin->setEmail('test@gmail.com');
-        $userAdmin->setEnabled(1);
+        $userManager = $this->container->get('fos_user.user_manager');
 
-        $manager->persist($userAdmin);
+        // Create a new user
+        $user = $userManager->createUser();
+        $user->setUsername('admin');
+        $user->setPlainPassword('test');
+        $user->setEmail('test@gmail.com');
+        $user->setEnabled(1);
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
