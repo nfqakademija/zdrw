e8e9599c63275b9809b15aba192dc170c6f15ae7
Ugnius Sinonis <ugniushead@gmail.com>
2014-11-04 19:16:10 +0200

    Adding RewardsData fixture

---
+++ src/Zdrw/OffersBundle/DataFixtures/ORM/RewardsData.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Zdrw\OffersBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Zdrw\OffersBundle\Entity\Offer;
+
+class LoadRewardsData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        // First offer
+        $offer = new Offer();
+
+        $user = $manager->getRepository('ZdrwUserBundle:User')->findOneByUsername('admin');
+        $offer->setOwner($user);
+        $offer->setPoints(500);
+
+        $manager->persist($offer3);
+        $manager->flush();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 3; // the order in which fixtures will be loaded
+    }
+}
