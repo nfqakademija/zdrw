5136886235fea29781c1186acb3ae7b2f4371b7c
Ugnius Sinonis <ugniushead@gmail.com>
2014-11-14 01:37:47 +0200

    Adding phpdocs to default controller

--- src/Zdrw/OffersBundle/Controller/DefaultController.php
+++ src/Zdrw/OffersBundle/Controller/DefaultController.php
@@ -10,30 +10,54 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * Function rendering the main project page
+     * Method rendering the main project page
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-
     public function indexAction()
     {
         return $this->render("ZdrwOffersBundle:Default:index.html.twig", array('user' => $this->getUser()));
     }
+
+    /**
+     * Method rendering the "Dares" page with all dares
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function daresAction()
     {
         $dares = $this->getDoctrine()->getRepository('ZdrwOffersBundle:Offer')->findAll();
         return $this->render('ZdrwOffersBundle:Default:dares.html.twig', array('dares' => $dares, 'user'=> $this->getUser()));
     }
+
+    /**
+     * Method rendering certain dare page
+     *
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function dareAction($id)
     {
         $dare = $this->getDoctrine()->getRepository('ZdrwOffersBundle:Offer')->findOneById($id);
         return $this->render("ZdrwOffersBundle:Default:dare.html.twig", array('dare' => $dare, 'user' => $this->getUser()));
     }
+
+    /**
+     * Method rendering the "Stares" page with all stares
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function staresAction()
     {
         $stares = $this->getDoctrine()->getRepository('ZdrwOffersBundle:Offer')->findAll();
         return $this->render('ZdrwOffersBundle:Default:stares.html.twig', array('stares' => $stares, 'user' => $this->getUser()));
     }
+
+    /**
+     * Method rendering user profile page with that user data
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function profileAction()
     {
         $user = $this->getUser();
