5564834600c7472c19394976e1aa99e38c4d2f9e
Ugnius Sinonis <ugniushead@gmail.com>
2014-11-16 03:45:03 +0200

    Changing styles, hidding the left sidebar for small screen gadgets.

--- app/Resources/scripts/leftMeniu.js
+++ app/Resources/scripts/leftMeniu.js
@@ -3,6 +3,7 @@
 
     var myToggle = 0;
     $('.left-sidebar-button').click(function(){
+
         var contentOffset = $('.page-content').offset().left;
         var pixelsLeft = 200 - contentOffset;
 
@@ -11,10 +12,10 @@
                 left: marginLeft
             }, 300, function() {
                 myToggle += 1;
-            });
-            $('.left-sidebar-button').animate({
-                marginLeft: buttonMarginLeft
-            }, 300, function() {
+                $('.left-sidebar-button').animate({
+                    marginLeft: buttonMarginLeft
+                }, 300, function() {
+                });
             });
             if (contentOffset < 200) {
                 $('.page-content').animate({
@@ -25,10 +26,18 @@
         }
 
         if(myToggle%2 === 0){
-            slideBar('0', '-30', pixelsLeft);
+            slideBar('0', '-32', pixelsLeft);
         }
         else{
             slideBar('-200','0', '15');
+        }
+    });
+    $( window ).resize(function() {
+        if($(window).width() < 755){
+            $('.left-sidebar').hide();
+        }
+        else{
+            $('.left-sidebar').show();
         }
     });
 })(window.jQuery);
