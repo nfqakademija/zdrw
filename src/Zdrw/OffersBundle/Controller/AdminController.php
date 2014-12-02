519b7791cee5410cc8de636075f77870be2ca5d8
Ugnius Sinonis <ugniushead@gmail.com>
2014-12-02 23:28:58 +0200

    Creating admin feature to add points to users

--- src/Zdrw/OffersBundle/Controller/AdminController.php
+++ src/Zdrw/OffersBundle/Controller/AdminController.php
@@ -2,31 +2,72 @@
 
 namespace Zdrw\OffersBundle\Controller;
 
-use Zdrw\OffersBundle\Entity\Reward;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Zdrw\OffersBundle\Entity\Offer;
+use Zdrw\UserBundle\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
-use Zdrw\OffersBundle\Form\Type\OfferType;
-use Zdrw\OffersBundle\Services\UserInfoProvider;
 
 /**
  * Controller managing the offers
  */
 class AdminController extends Controller
 {
-    public function indexAction()
+    /**
+     * Method rendering admin panel page
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request)
     {
         if ($this->get('security.context')->isGranted('ROLE_ADMIN')) {
             $stares = $this->getDoctrine()->getRepository('ZdrwOffersBundle:Offer')->findBy(array('status' => 4));
+            $points = 0;
+            $userPoints = 0;
+            $userNick = 0;
+
+            $user = new User();
+
+            $form = $this->createFormBuilder($user)
+                ->add('nickname', 'text')
+                ->add('points', 'integer')
+                ->add('save', 'submit', array('label' => 'Add points'))
+                ->getForm();
+
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $data = $form->getData();
+                $userNick = $data->getNickname();
+                $user = $em->getRepository('ZdrwUserBundle:User')
+                    ->findOneBy(
+                        array(
+                            'nickname' => $userNick
+                        )
+                    );
+                if (!$user) {
+                    $points = 2;
+                } else {
+                    $points = $user->getPoints();
+                    $userPoints = $points + $data->getPoints();
+                    $user->setPoints($userPoints);
+                    $em->persist($user);
+                    $em->flush();
+
+                    $points = 1;
+                }
+            }
             return $this->render(
                 'ZdrwOffersBundle:Admin:index.html.twig',
                 array(
-                    'user' => $this->getUser(), 'stares' => $stares
+                    'user' => $this->getUser(),
+                    'stares' => $stares,
+                    'form' => $form->createView(),
+                    'points' => $points,
+                    'userPoints' => $userPoints,
+                    'userNick' => $userNick
                 )
             );
-        }
-        else{
-            return $this->redirect($this->generateUrl('zdrw_index'),301);
+        } else {
+            return $this->redirect($this->generateUrl('zdrw_index'), 301);
         }
     }
 }
