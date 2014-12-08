b538a57074af98291a031c2d6f7686f746725015
faik <faik900@gmail.com>
2014-12-08 21:09:31 +0200

    Changes to profile edition form: removed username field; added nickname field;

---
+++ src/Zdrw/UserBundle/Form/Type/ProfileFormType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Zdrw\UserBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
+
+class ProfileFormType extends BaseType
+{
+
+    public function getParent()
+    {
+        return 'fos_user_profile';
+    }
+    public function getName()
+    {
+        return 'zdrw_profile';
+    }
+    /**
+     * Builds the embedded form representing the user.
+     *
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    protected function buildUserForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nickname', null, array('label' => 'Nickname', 'translation_domain' => 'FOSUserBundle'))
+            ->add('email', 'email', array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
+        ;
+    }
+}
