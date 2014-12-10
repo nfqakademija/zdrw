935a69eb5e86b72afa112181f392c5084e4bce3c
Ugnius Sinonis <ugniushead@gmail.com>
2014-12-10 12:51:25 +0200

    Front-end changes; Mobile gadges login redirect

---
+++ app/Resources/scripts/loginLink.js
@@ -0,0 +1,10 @@
+(function($){
+    'use strict';
+
+    $('#login-link').click(function(){
+        var link = $(this).data('link');
+        if($(window).width() < 768) {
+            window.location.replace(link);
+        }
+    });
+})(jQuery);
