8fc759fb0aedbe432fa157250dbd8ecf9758cafb
Ugnius Sinonis <ugniushead@gmail.com>
2014-12-26 23:08:00 +0200

    Integrating paysera(1st step)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,7 +21,8 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Zdrw\UserBundle\ZdrwUserBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
-            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle()
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new Evp\Bundle\WebToPayBundle\EvpWebToPayBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
